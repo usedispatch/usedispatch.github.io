cfc04c78d5f448b86f194bae86499d829a4be712
viksit <viksit@gmail.com>
2022-01-27 15:36:34 -0800

    update to read dispatch

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `Dispatch Protocol`,
+    siteTitleAlt: `Dispatch Protocol`,
+    siteHeadline: `Dispatch Protocol`,
+    siteUrl: `https://www.usedispatch.net`,
+    siteDescription: `The Dispatch Protocol on Solana helps dapps communicate with their users in a fast, reliable and secure manner.`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@viksit`,
   },
   plugins: [
     {
@@ -25,9 +25,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `Dispatch Protocol`,
+        short_name: `Dispatch`,
+        description: `The Dispatch Protocol on Solana helps dapps communicate with their users in a fast, reliable and secure manner.`,
         start_url: `/`,
         background_color: `#141821`,
         // This will impact how browsers show your PWA/website
